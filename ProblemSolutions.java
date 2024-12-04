2c761e121b84d2b1094213c3e44b60e9d02c1261
megluc <140560593+megluc@users.noreply.github.com>
2024-12-03 23:21:11 -0600

    Update ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,117 +1,141 @@
-/******************************************************************
- *
- *   ADD YOUR NAME / SECTION NUMBER HERE
- *
- *   This java file contains the problem solutions of canFinish and
- *   numGroups methods.
- *
- ********************************************************************/
+
+    /******************************************************************
+     *
+     *   Meghan Garcia / SECTION 002
+     *
+     *   This java file contains the problem solutions of canFinish and
+     *   numGroups methods.
+     *
+     ********************************************************************/
 
 import java.util.*;
 
-class ProblemSolutions {
+    class ProblemSolutions {
 
-    /**
-     * Method canFinish
-     *
-     * You are building a course curriculum along with required intermediate
-     * exams certifications that must be taken by programmers in order to obtain
-     * a new certification called 'master programmer'. In doing so, you are placing
-     * prerequisites on intermediate exam certifications that must be taken first.
-     * You are allowing the flexibility of taking the exams in any order as long as
-     * any exam prerequisites are satisfied.
-     *
-     * Unfortunately, in the past, your predecessors have accidentally published
-     * curriculums and exam schedules that were not possible to complete due to cycles
-     * in prerequisites. You want to avoid this embarrassment by making sure you define
-     * a curriculum and exam schedule that can be completed.
-     *
-     * You goal is to ensure that any student pursuing the certificate of 'master
-     * programmer', can complete 'n' certification exams, each being specific to a
-     * topic. Some exams have prerequisites of needing to take and pass earlier
-     * certificate exams. You do not want to force any order of taking the exams, but
-     * you want to make sure that at least one order is possible.
-     *
-     * This method will save your embarrassment by returning true or false if
-     * there is at least one order that can taken of exams.
-     *
-     * You wrote this method, and in doing so, you represent these 'n' exams as
-     * nodes in a graph, numbered from 0 to n-1. And you represent the prerequisite
-     * between taking exams as directed edges between two nodes which represent
-     * those two exams.
-     *
-     * Your method expects a 2-dimensional array of exam prerequisites, where
-     * prerequisites[i] = [ai, bi] indicating that you must take exam 'bi' first
-     * if you want to take exam 'ai'. For example, the pair [0, 1], indicates that
-     * to take exam certification '0', you have to first have the certification for
-     * exam '1'.
-     *
-     * The method will return true if you can finish all certification exams.
-     * Otherwise, return false (e.g., meaning it is a cyclic or cycle graph).
-     *
-     *     Example 1:
-     *     Input: numExams = 2, prerequisites = [[1,0]]
-     *     Output: true
-     *     Explanation: There are a total of 2 exams to take.
-     *     To take exam 1 you should have completed the
-     *     certification of exam 0. So, it is possible (no
-     *     cyclic or cycle graph of prereqs).
-     *
-     *
-     *     Example 2:
-     *     Input: numExams = 2, prerequisites = [[1,0],[0,1]]
-     *     Output: false
-     *     Explanation: There are a total of 2 exams to take.
-     *     To take exam 1 you should have completed the
-     *     certification of exam 0, and to take exams 0 you
-     *     should also have completed the certification of exam
-     *     1. So, it is impossible (it is a cycle graph).
-     *
-     * @param numExams          - number of exams, which will produce a graph of n nodes
-     * @param prerequisites     - 2-dim array of directed edges.
-     * @return boolean          - True if all exams can be taken, else false.
-     */
+        /**
+         * Method canFinish
+         *
+         * You are building a course curriculum along with required intermediate
+         * exams certifications that must be taken by programmers in order to obtain
+         * a new certification called 'master programmer'. In doing so, you are placing
+         * prerequisites on intermediate exam certifications that must be taken first.
+         * You are allowing the flexibility of taking the exams in any order as long as
+         * any exam prerequisites are satisfied.
+         *
+         * Unfortunately, in the past, your predecessors have accidentally published
+         * curriculums and exam schedules that were not possible to complete due to cycles
+         * in prerequisites. You want to avoid this embarrassment by making sure you define
+         * a curriculum and exam schedule that can be completed.
+         *
+         * You goal is to ensure that any student pursuing the certificate of 'master
+         * programmer', can complete 'n' certification exams, each being specific to a
+         * topic. Some exams have prerequisites of needing to take and pass earlier
+         * certificate exams. You do not want to force any order of taking the exams, but
+         * you want to make sure that at least one order is possible.
+         *
+         * This method will save your embarrassment by returning true or false if
+         * there is at least one order that can taken of exams.
+         *
+         * You wrote this method, and in doing so, you represent these 'n' exams as
+         * nodes in a graph, numbered from 0 to n-1. And you represent the prerequisite
+         * between taking exams as directed edges between two nodes which represent
+         * those two exams.
+         *
+         * Your method expects a 2-dimensional array of exam prerequisites, where
+         * prerequisites[i] = [ai, bi] indicating that you must take exam 'bi' first
+         * if you want to take exam 'ai'. For example, the pair [0, 1], indicates that
+         * to take exam certification '0', you have to first have the certification for
+         * exam '1'.
+         *
+         * The method will return true if you can finish all certification exams.
+         * Otherwise, return false (e.g., meaning it is a cyclic or cycle graph).
+         *
+         *     Example 1:
+         *     Input: numExams = 2, prerequisites = [[1,0]]
+         *     Output: true
+         *     Explanation: There are a total of 2 exams to take.
+         *     To take exam 1 you should have completed the
+         *     certification of exam 0. So, it is possible (no
+         *     cyclic or cycle graph of prereqs).
+         *
+         *
+         *     Example 2:
+         *     Input: numExams = 2, prerequisites = [[1,0],[0,1]]
+         *     Output: false
+         *     Explanation: There are a total of 2 exams to take.
+         *     To take exam 1 you should have completed the
+         *     certification of exam 0, and to take exams 0 you
+         *     should also have completed the certification of exam
+         *     1. So, it is impossible (it is a cycle graph).
+         *
+         * @param numExams          - number of exams, which will produce a graph of n nodes
+         * @param prerequisites     - 2-dim array of directed edges.
+         * @return boolean          - True if all exams can be taken, else false.
+         */
 
-    public boolean canFinish(int numExams, 
-                             int[][] prerequisites) {
-      
-        int numNodes = numExams;  // # of nodes in graph
+        public boolean canFinish(int numExams,
+                                 int[][] prerequisites) {
+            int[] inDegree = new int[numExams]; // Track in-degrees of all nodes
+            ArrayList<Integer>[] adj = getAdjList(numExams, prerequisites);
 
-        // Build directed graph's adjacency list
-        ArrayList<Integer>[] adj = getAdjList(numExams, 
-                                        prerequisites); 
+            // Calculate in-degrees
+            for (int[] prereq : prerequisites) {
+                inDegree[prereq[0]]++;
+            }
 
-        // ADD YOUR CODE HERE - ADD YOUR NAME / SECTION AT TOP OF FILE
-        return false;
+            // Use a queue to process nodes with 0 in-degree
+            Queue<Integer> queue = new LinkedList<>();
+            for (int i = 0; i < numExams; i++) {
+                if (inDegree[i] == 0) {
+                    queue.offer(i);
+                }
+            }
 
-    }
+            int visitedNodes = 0; // Count nodes visited in topological order
+            while (!queue.isEmpty()) {
+                int current = queue.poll();
+                visitedNodes++;
+
+                for (int neighbor : adj[current]) {
+                    inDegree[neighbor]--; // Reduce in-degree of neighbor
+                    if (inDegree[neighbor] == 0) {
+                        queue.offer(neighbor);
+                    }
+                }
+            }
+
+            // If all nodes are visited, it means no cycles exist
+            return visitedNodes == numExams;
+        }
 
 
-    /**
-     * Method getAdjList
-     *
-     * Building an Adjacency List for the directed graph based on number of nodes
-     * and passed in directed edges.
-     *
-     * @param numNodes      - number of nodes in graph (labeled 0 through n-1) for n nodes
-     * @param edges         - 2-dim array of directed edges
-     * @return ArrayList<Integer>[]  - An adjacency list representing the provided graph.
-     */
 
-    private ArrayList<Integer>[] getAdjList(
-            int numNodes, int[][] edges) {
 
-        ArrayList<Integer>[] adj 
+        /**
+         * Method getAdjList
+         *
+         * Building an Adjacency List for the directed graph based on number of nodes
+         * and passed in directed edges.
+         *
+         * @param numNodes      - number of nodes in graph (labeled 0 through n-1) for n nodes
+         * @param edges         - 2-dim array of directed edges
+         * @return ArrayList<Integer>[]  - An adjacency list representing the provided graph.
+         */
+
+        private ArrayList<Integer>[] getAdjList(
+                int numNodes, int[][] edges) {
+
+            ArrayList<Integer>[] adj
                     = new ArrayList[numNodes];      // Create an array of ArrayList ADT
 
-        for (int node = 0; node < numNodes; node++){
-            adj[node] = new ArrayList<Integer>();   // Allocate empty ArrayList per node
+            for (int node = 0; node < numNodes; node++){
+                adj[node] = new ArrayList<Integer>();   // Allocate empty ArrayList per node
+            }
+            for (int[] edge : edges){
+                adj[edge[0]].add(edge[1]);              // Add connected node edge [1] for node [0]
+            }
+            return adj;
         }
-        for (int[] edge : edges){
-            adj[edge[0]].add(edge[1]);              // Add connected node edge [1] for node [0]
-        }
-        return adj;
-    }
 
 
     /*
@@ -163,36 +187,27 @@ class ProblemSolutions {
      *   edge. So they form on large group.
      */
 
-    public int numGroups(int[][] adjMatrix) {
-        int numNodes = adjMatrix.length;
-        Map<Integer,List<Integer>> graph = new HashMap();
-        int i = 0, j =0;
+        public int numGroups(int[][] adjMatrix) {
+            int n = adjMatrix.length;
+            boolean[] visited = new boolean[n];
+            int groupCount = 0;
 
-        /*
-         * Converting the Graph Adjacency Matrix to
-         * an Adjacency List representation. This
-         * sample code illustrates a technique to do so.
-         */
+            for (int i = 0; i < n; i++) {
+                if (!visited[i]) {
+                    groupCount++;
+                    dfs(adjMatrix, i, visited);
+                }
+            }
 
-        for(i = 0; i < numNodes ; i++){
-            for(j = 0; j < numNodes; j++){
-                if( adjMatrix[i][j] == 1 && i != j ){
-                    // Add AdjList for node i if not there
-                    graph.putIfAbsent(i, new ArrayList());
-                    // Add AdjList for node j if not there
-                    graph.putIfAbsent(j, new ArrayList());
+            return groupCount;
+        }
 
-                    // Update node i adjList to include node j
-                    graph.get(i).add(j);
-                    // Update node j adjList to include node i
-                    graph.get(j).add(i);
+        private void dfs(int[][] adjMatrix, int node, boolean[] visited) {
+            visited[node] = true;
+            for (int neighbor = 0; neighbor < adjMatrix[node].length; neighbor++) {
+                if (adjMatrix[node][neighbor] == 1 && !visited[neighbor]) {
+                    dfs(adjMatrix, neighbor, visited);
                 }
             }
         }
-
-        // YOUR CODE GOES HERE - you can add helper methods, you do not need
-        // to put all code in this method.
-        return -1;
     }
-
-}
